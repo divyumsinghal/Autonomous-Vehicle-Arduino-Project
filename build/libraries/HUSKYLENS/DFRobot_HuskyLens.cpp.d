4d33694635299e7e446616e12b4849eaf51431b9
Divyum Singhal <divyumsinghal@gmail.com>
2024-03-21 16:36:09 +0000

    need to test the left and right movement for gold on track, tag 6 and 7, tags 1 to 5 work fine

--- build/libraries/HUSKYLENS/DFRobot_HuskyLens.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\divyu\OneDrive\ -\ Trinity\ College\ Dublin\Desktop\Buggy\Autonomous-Vehicle-Arduino-Project\build\libraries\HUSKYLENS\DFRobot_HuskyLens.cpp.o: \
- C:\Users\divyu\OneDrive\ -\ Trinity\ College\ Dublin\Documents\Arduino\libraries\HUSKYLENS\DFRobot_HuskyLens.cpp
